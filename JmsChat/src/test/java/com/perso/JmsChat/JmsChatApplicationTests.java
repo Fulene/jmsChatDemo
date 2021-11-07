157daf64acf310434f8d37ea69f53e2b00eeac53
mhamerlaine <mhamerlaine@synox.io>
2021-11-07 23:06:33 +0100

    Initial commit

---
+++ JmsChat/src/test/java/com/perso/JmsChat/JmsChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perso.JmsChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
